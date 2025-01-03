6d81a70cae799c40d8d1b173f17f768d33a858fb
Ge <geovanasslima@gmail.com>
2025-01-03 00:24:33 -0300

    Creating reservations

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import uploadConfig from './config/upload';
 import SessionController from './controllers/SessionController';
 import HouseController from './controllers/HouseController';
 import DashboardController from './controllers/DashboardController';
+import ReservationController from './controllers/ReservationController';
 
 const routes = new Router();
 const upload = multer(uploadConfig);
@@ -17,5 +18,7 @@ routes.delete("/houses", HouseController.destroy);
 routes.put("/houses/:house_id", upload.single('thumbnail'), HouseController.update);
 
 routes.get("/dashboard", DashboardController.show);
+
+routes.post("/houses/:house_id/reserve", ReservationController.store);
 
 export default routes;
