05b8188b3d4926dfbdb0e5df1b82c1ea8585e1a5
Ge <geovanasslima@gmail.com>
2025-01-02 23:33:38 -0300

    Adding isOwner util

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -1,5 +1,6 @@
 import House from "../models/House";
 import User from "../models/User";
+import { isOwner } from "../config/utils";
 
 class HouseController {
 
@@ -29,43 +30,55 @@ class HouseController {
   }
 
   async update(req, res) {
-    const { filename } = req.file;
-    const { house_id } = req.params;
-    const { user_id } = req.headers;
-    const { description, price, location, available } = req.body;
-
-    const user = await User.findById(user_id);
-    const house = await House.findById(house_id);
-
-    if(String(user._id) !== String(house.user)) {
-      return res.status(401).json({ error: "Não autorizado." });
+    try {
+      const { filename } = req.file;
+      const { house_id } = req.params;
+      const { user_id } = req.headers;
+      const { description, price, location, available } = req.body;
+  
+      const owner = await isOwner(user_id, house_id);
+  
+      if (!owner) {
+        return res.status(401).json({ error: "Não autorizado." });
+      }
+  
+      await House.updateOne(
+        { _id: house_id },
+        {
+          user: user_id,
+          thumbnail: filename,
+          description,
+          price,
+          location,
+          available,
+        }
+      );
+  
+      return res.status(200).json({ message: "Casa atualizada com sucesso." });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: "Erro interno do servidor." });
     }
-
-    await House.updateOne({ _id: house_id }, {
-      user: user_id,
-      thumbnail: filename,
-      description,
-      price,
-      location,
-      available
-    })
-    return res.send();
   }
 
   async destroy(req, res) {
-    const { house_id } = req.body;
-    const { user_id } = req.headers;
+    try {
+      const { house_id } = req.body;
+      const { user_id } = req.headers;
 
-    const user = await User.findById(user_id);
-    const house = await House.findById(house_id);
+      const owner = await isOwner(user_id, house_id);
+  
+      if (!owner) {
+        return res.status(401).json({ error: "Não autorizado." });
+      }
 
-    if(String(user._id) !== String(house.user)) {
-      return res.status(401).json({ error: "Não autorizado." });
+      await House.findByIdAndDelete({ _id: house_id });
+
+      return res.status(200).json({ message: "Casa excluída com sucesso."});
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: "Erro interno do servidor." });
     }
-
-    await House.findByIdAndDelete({ _id: house_id });
-
-    return res.json({ message: "Casa excluída com sucesso."});
   }
 }
 
